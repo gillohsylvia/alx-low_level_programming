3066c359c7fac563fc791d62fa93967482c8cd85
gillohsylvia <abdallahfatima363@gmail.com>
2022-06-02 03:47:55 -0700

    my first program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 
-
-
-/**                                                                           
- *main - Entry point                                                         
+/**
+ *main - Entry point
  *
- *Return: Always 0 (Success)                                                                            
+ * Return: Always 0 (Success)
  *
  */
-
 
 
 int main(void)
