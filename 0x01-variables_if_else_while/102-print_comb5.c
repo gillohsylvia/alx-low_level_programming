7bb96ba43a9dafbf44e5664036109d28ef0a298b
gillohsylvia <abdallahfatima363@gmail.com>
2022-06-03 16:00:33 -0700

    Printing all three possible numbers

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - all codes
+ *
+ * Description: Printing all possible combinations of two digits numbers
+ *
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 100; i++)
+	{
+		for (j = 0; j < 100; j++)
+		{
+			if (i < j)
+			{
+				putchar((i / 10) + 48);
+				putchar((i % 10) + 48);
+				putchar(' ');
+				putchar((j / 10) + 48);
+				putchar((j % 10) + 48);
+				if (i != 98 || j != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
