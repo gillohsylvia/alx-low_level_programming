d78e75e4036f297041529ee258d347396233f776
gillohsylvia <abdallahfatima363@gmail.com>
2022-06-03 10:15:15 -0700

    Printing alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - all codes
+ *
+ * Description: A program
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+
+{
+char alphabets;
+char ch;
+ch = '\n';
+	for (alphabets = 'a'; alphabets <= 'z'; alphabets++)
+	{
+	putchar(alphabets);
+	}
+putchar(ch);
+return (0);
+}
