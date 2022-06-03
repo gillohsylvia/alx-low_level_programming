287d875b1b0111eb63b3986309835d257383d388
gillohsylvia <abdallahfatima363@gmail.com>
2022-06-03 12:38:32 -0700

    Printing lower case except q and e

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,16 +8,14 @@
  *
  */
 int main(void)
-{
-	char alphabets;
 
-	for (alphabets = 'a'; alphabets <= 'z'; alphabets++)
+{
+	char alphabet;
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 	{
-	if (alphabets != 'e' && alphabets != 'q')
-	{
-	putchar(alphabets);
+		if (alphabet != 'e' && alphabet != 'q')
+			putchar(alphabet);
 	}
 	putchar('\n');
-	}
 	return (0);
 }
