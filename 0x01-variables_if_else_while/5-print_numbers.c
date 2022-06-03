1819cf780dcbdb691f963e5be33c5e05496f18e6
gillohsylvia <abdallahfatima363@gmail.com>
2022-06-03 12:54:01 -0700

    Printing numbers from 0-10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *main - all codes
+ *Description: Printing all single numbers
+ *
+ *Return: 0
+ *
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 0; number < 10; number++)
+	{
+	printf("%i", number);
+	putchar('\n');
+	}
+	return (0);
+}
