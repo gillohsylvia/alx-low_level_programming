1c1b3501f43c83191e4a6871e3d2510efa419828
gillohsylvia <abdallahfatima363@gmail.com>
2023-01-08 08:16:55 +0300

    I love it too

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -10,5 +10,7 @@ int binary_search(int *array, size_t size, int value);
 int linear_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
+int binary_search_recursive(int *array, int value, int start, int end);
 
 #endif
